f4c2c8bff9330d81bd03e36e62f882a96fa6f44f
JPantana <joshpantana@me.com>
2019-04-13 17:25:23 -0500

    wip

--- javascripts/components/sandwich.js
+++ javascripts/components/sandwich.js
@@ -1,12 +1,12 @@
 import meat from "./meat.js";
 import util from "../helpers/util.js";
-import buttonEvents from "../helpers/buttonEvents.js";
+// import buttonEvents from "../helpers/buttonEvents.js";
 
 // const ingredients = [];
 const cart = [];
 
 const mySandwich = (e) => {
-    e.preventDefault(); 
+    // e.preventDefault(); 
     let meats = meat.addMeat();
     console.log(meats);
     let eventId = e.target.id;
@@ -22,18 +22,23 @@ const mySandwich = (e) => {
     }
         const sandTotal = cart.reduce((a, b) => {return a + b.price}, 0);
         let domString = '';   
-                domString += `<div>`;
+                domString += `<div class="card m-auto ingredCard">`;
                 domString += `  <h3>Ingredients:</h3>`;
                 domString += `  <ul>`;
                 cart.forEach((item) => {
-                domString += `           <li>${cart.name},</li>`;
+                    console.log(item.name);
+                domString += `           <li>${item.name}</li>`;
             })
                 domString += `      </ul>`;
                 domString += `</div>`;
                 domString += `<div>`;
-                domString += `   <h3 class="">Total Cost: <span id="costSpan">$${sandTotal.toFixed(2)}</span></h3>`;
+                domString += `  <h3 class="card priceCard">Total Cost: <span id="costSpan">$${sandTotal.toFixed(2)}</span></h3>`;
+                domString += `  </div class="d-flex ml-auto">`;
+                domString += `      <button id="checkoutBtn" class="btn btn-danger checkoutBtn" type="submit">Checkout</button>`;
+                domString += `  </div>`
                 domString += `</div>`;
-                
+
+
         util.printToDom('totalCostDiv', domString);
 };
 
